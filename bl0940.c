d75c6ca608f8f953df9b4872c366c7b30e69eec2
wu bobo <wuwbobo@outlook.com>
2023-09-03 04:58:11 +0800

    Add files via upload

---
+++ bl0940.c
@@ -0,0 +1,199 @@
+// by wuwbobo2021 <https://github.com/wuwbobo2021>, <wuwbobo@outlook.com>
+// If you have found bugs in this program, please pull an issue, or contact me.
+
+#include "bl0940.h"
+
+#include <math.h>
+#define PI		3.14159265358979
+
+#define BL0940_Head_Read                    0x58
+#define BL0940_Head_Write                   0xA8
+
+#define BL0940_Reg_Addr_Write_Protect       0x1A
+#define BL0940_Unlock_User_Reg              0x55 //write to the write protect register
+
+#define BL0940_Reg_Addr_Mode_Settings       0x18
+#define BL0940_Reg_Mode_Pos_I_Filter        0
+#define BL0940_Reg_Mode_Pos_V_Filter        4
+#define BL0940_Reg_Mode_Pos_Av_Time         8
+#define BL0940_Reg_Mode_Pos_AC_Freq         9
+
+#define BL0940_Get_Readings                 0xAA //at the position of register index
+#define BL0940_Head_Readings                0x55
+#define BL0940_Len_Readings                 35
+
+// each of them takes 3 bytes (little endian)
+#define BL0940_Readings_Pos_I_Fast           1 //TODO
+#define BL0940_Readings_Pos_I                4
+#define BL0940_Readings_Pos_V               10
+#define BL0940_Readings_Pos_W               16
+#define BL0940_Readings_Pos_Pulses          22
+#define BL0940_Readings_Pos_Temp_Internal   28
+#define BL0940_Readings_Pos_Temp_External   31
+
+#define BL0940_Vref                         1.218
+
+#define BL0940_Coefficient_V                (BL0940_Vref/79931.0/1000)
+#define BL0940_Coefficient_I                (BL0940_Vref/324004.0/1000)
+#define BL0940_Coefficient_W                (BL0940_Vref*BL0940_Vref/4046.0/1000.0/1000)
+#define BL0940_Coefficient_kWh              (BL0940_Coefficient_W * 1638.4*256/3600000)
+
+#define BL0940_Reg_Addr_Angle               0x0C
+#define BL0940_Freq_Sample                  1000000
+
+#define BL0940_Timeout_ms                   500
+
+static uint32_t read_register(BL0940* bl0940, uint8_t reg_addr);
+static bool write_register(BL0940* bl0940, uint8_t reg_addr, uint32_t val);
+static uint16_t bytes_sum(void* data, uint8_t cnt);
+static void set_triple_bytes(uint8_t* data, uint32_t val);
+static uint32_t get_triple_bytes(const uint8_t* data);
+static void set_bits(uint32_t* p_reg, uint32_t val, uint8_t pos, uint8_t size);
+
+bool bl0940_apply_settings(BL0940* bl0940)
+{
+	bl0940->error = BL0940_Error_None;
+	
+	uint32_t reg_mode = read_register(bl0940, BL0940_Reg_Addr_Mode_Settings);
+	if (bl0940->error) return false;
+	
+	set_bits(&reg_mode, bl0940->setting_filter, BL0940_Reg_Mode_Pos_I_Filter, 2);
+	set_bits(&reg_mode, bl0940->setting_filter, BL0940_Reg_Mode_Pos_V_Filter, 2);
+	set_bits(&reg_mode, bl0940->setting_av_time, BL0940_Reg_Mode_Pos_Av_Time, 1);
+	set_bits(&reg_mode, bl0940->setting_ac_freq, BL0940_Reg_Mode_Pos_AC_Freq, 1);
+	
+	return write_register(bl0940, BL0940_Reg_Addr_Mode_Settings, reg_mode);
+}
+
+bool bl0940_get_readings(BL0940* bl0940)
+{
+	bl0940->error = BL0940_Error_None;
+	
+	char tx_data[] = {BL0940_Head_Read, BL0940_Get_Readings};
+	if (! bl0940_uart_send(bl0940->uart_port_num, tx_data, sizeof(tx_data))) {
+		bl0940->error = BL0940_Error_Write; return false;
+	}
+	
+	char rx_data[BL0940_Len_Readings]; uint8_t cnt_rx;
+	cnt_rx = bl0940_uart_receive(bl0940->uart_port_num, rx_data,
+	                             sizeof(rx_data), BL0940_Timeout_ms);
+	if (cnt_rx < sizeof(rx_data)) {
+		bl0940->error = BL0940_Error_Read_Len; return false;
+	}
+	
+	uint16_t sum = BL0940_Head_Read;
+	sum += bytes_sum(rx_data, sizeof(rx_data) - 1);
+	uint8_t* p_crc = &(rx_data[sizeof(rx_data) - 1]);
+	if (*p_crc != sum & 0xFF) {
+		bl0940->error = BL0940_Error_Read_Crc; return false;
+	}
+	
+	bl0940->voltage =   get_triple_bytes(rx_data + BL0940_Readings_Pos_V)
+	                  * BL0940_Coefficient_V * bl0940->voltage_divider;
+	
+	bl0940->current =   get_triple_bytes(rx_data + BL0940_Readings_Pos_I)
+	                  * BL0940_Coefficient_I / bl0940->r_shunt_ohm;
+	
+	bl0940->power =   get_triple_bytes(rx_data + BL0940_Readings_Pos_W)
+	                * BL0940_Coefficient_W * bl0940->voltage_divider / bl0940->r_shunt_ohm;
+	
+	bl0940->energy =   get_triple_bytes(rx_data + BL0940_Readings_Pos_Pulses)
+	                 * BL0940_Coefficient_kWh * bl0940->voltage_divider / bl0940->r_shunt_ohm;
+	
+	bl0940->temp_internal = get_triple_bytes(rx_data + BL0940_Readings_Pos_Temp_Internal);
+	bl0940->temp_internal = (170.0/448)*(bl0940->temp_internal/2 - 32) - 45;
+	
+	float data_angle = read_register(bl0940, BL0940_Reg_Addr_Angle) & 0xFFFF;
+	if (bl0940->error) return false;
+	
+	data_angle *= (bl0940->setting_ac_freq == BL0940_AC_Freq_50Hz ? 50 : 60);
+	data_angle /= BL0940_Freq_Sample;
+	bl0940->phase_angle = 360.0*data_angle;
+	bl0940->power_factor = cos(2*PI*data_angle);
+	
+	return true;
+}
+
+static uint32_t read_register(BL0940* bl0940, uint8_t reg_addr)
+{
+	char tx_data[] = {BL0940_Head_Read, reg_addr};
+	if (! bl0940_uart_send(bl0940->uart_port_num, tx_data, sizeof(tx_data))) {
+		bl0940->error = BL0940_Error_Write; return 0;
+	}
+	
+	char rx_data[4]; uint8_t cnt_rx;
+	cnt_rx = bl0940_uart_receive(bl0940->uart_port_num, rx_data,
+	                             sizeof(rx_data), BL0940_Timeout_ms);
+	if (cnt_rx < sizeof(rx_data)) {
+		bl0940->error = BL0940_Error_Read_Len; return 0;
+	}
+	
+	uint16_t sum = 0;
+	sum += bytes_sum(tx_data, sizeof(tx_data));
+	sum += bytes_sum(rx_data, sizeof(rx_data) - 1);
+	
+	uint8_t* p_crc = &(rx_data[sizeof(rx_data) - 1]);
+	if (*p_crc != sum & 0xFF) {
+		bl0940->error = BL0940_Error_Read_Crc; return 0;
+	}
+	
+	return get_triple_bytes(rx_data);
+}
+
+static bool write_register(BL0940* bl0940, uint8_t reg_addr, uint32_t val)
+{
+	if (reg_addr != BL0940_Reg_Addr_Write_Protect)
+		write_register(bl0940, BL0940_Reg_Addr_Write_Protect, BL0940_Unlock_User_Reg);
+	
+	char tx_data[6] = {BL0940_Head_Write, reg_addr};
+	set_triple_bytes(tx_data + 2, val);
+	uint8_t* p_crc = &(tx_data[sizeof(tx_data) - 1]);
+	*p_crc = bytes_sum(tx_data + 2, 3) & 0xFF;
+	
+	if (! bl0940_uart_send(bl0940->uart_port_num, tx_data, sizeof(tx_data))) {
+		bl0940->error = BL0940_Error_Write; return false;
+	}
+	
+	if (reg_addr == BL0940_Reg_Addr_Write_Protect)
+		return true;
+	
+	uint32_t reread = read_register(bl0940, reg_addr);
+	if (reread != val) {
+		bl0940->error = BL0940_Error_Write_Reg; return false;
+	}
+	
+	write_register(bl0940, BL0940_Reg_Addr_Write_Protect, 0x00);
+	return true;
+}
+
+static uint16_t bytes_sum(void* data, uint8_t cnt)
+{
+	if (data == (void*)0 || cnt == 0) return 0;
+	
+	uint8_t* p = (uint8_t*)data;
+	uint16_t sum;
+	for (int i = 0; i < cnt; i++)
+		sum += p[i];
+	return sum;
+}
+
+static uint32_t get_triple_bytes(const uint8_t* data)
+{
+	return ((uint32_t)data[2] << 16) | 
+           ((uint32_t)data[1] << 8) | 
+           ((uint32_t)data[0]);
+}
+
+static void set_triple_bytes(uint8_t* data, uint32_t val)
+{
+	data[0] = val & 0xFF;
+	data[1] = (val >> 8) & 0xFF;
+	data[2] = (val >> 16) & 0xFF;
+}
+
+static void set_bits(uint32_t* p_reg, uint32_t val, uint8_t pos, uint8_t size)
+{
+	uint32_t mask = (UINT32_MAX << (pos + size)) | (UINT32_MAX >> (32 - pos));
+	*p_reg &= mask;
+	*p_reg |= (val << pos) & (~mask);
+}
---
+++ bl0940.h
@@ -0,0 +1,73 @@
+// by wuwbobo2021 <https://github.com/wuwbobo2021>, <wuwbobo@outlook.com>
+// If you have found bugs in this program, please pull an issue, or contact me.
+
+#ifndef BL0940_H
+#define BL0940_H
+
+#ifdef __cplusplus
+extern "C" {
+#else
+#include <stdbool.h>
+#endif
+
+#include <stdint.h>
+
+typedef enum {
+	BL0940_Filter_AC_Pass = 0b00, //default
+	BL0940_Filter_DC_Pass = 0b10,
+	BL0940_Filter_All = 0b11
+} BL0940_Filter;
+
+typedef enum {
+	BL0940_Av_Time_400ms = 0, //default
+	BL0940_Av_Time_800ms = 1
+} BL0940_Av_Time;
+
+typedef enum {
+	BL0940_AC_Freq_50Hz = 0, //default
+	BL0940_AC_Freq_60Hz = 1
+} BL0940_AC_Freq;
+
+typedef enum {
+	BL0940_Error_None,
+	BL0940_Error_Read_Len,
+	BL0940_Error_Read_Crc,
+	BL0940_Error_Write,
+	BL0940_Error_Write_Reg
+} BL0940_Error;
+
+typedef struct {
+	uint8_t uart_port_num;
+	
+	float voltage_divider; // = (R2 + R1)/R1, the input is voltage on R1
+	float r_shunt_ohm; //for example: 0.001 ohm
+	
+	// call bl0940_apply_settings() after changing values below
+	BL0940_Filter setting_filter;
+	BL0940_Av_Time setting_av_time;
+	BL0940_AC_Freq setting_ac_freq;
+	
+	// values below are set by this module
+	BL0940_Error error;
+	
+	float voltage;
+	float current;
+	float phase_angle;
+	float power_factor;
+	float power;
+	float energy;
+	float temp_internal;
+} BL0940;
+
+// implement them as synchronous functions outside this module
+extern bool bl0940_uart_send(uint8_t port_num, char* data, uint8_t cnt);
+extern uint8_t bl0940_uart_receive(uint8_t port_num, char* data, uint8_t cnt, uint16_t timeout_ms);
+
+extern bool bl0940_apply_settings(BL0940* bl0940);
+extern bool bl0940_get_readings(BL0940* bl0940);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  //BL0940_H
